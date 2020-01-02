a00b877c991360c73b4c1d5deb3bc0a9bb2d9916
jamattard <jamattard@gmail.com>
2020-01-02 17:03:35 +0100

    Add WPF Project with MVVM
    Adds our WPF user interface project and configures it with Caliburn Micro for our MVVM framework. Also includes the Simple Container DI system

---
+++ RSDesktopUI/Bootstrapper.cs
@@ -0,0 +1,65 @@
+﻿using Caliburn.Micro;
+using RSDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RSDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        // Simple Container is part of Caliburn and is a a container for dependency injection
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        // override to use our DI container
+        protected override void Configure()
+        {
+            // whenever we ask for the container, it will return THIS container
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()  // service to manage windows
+                .Singleton<IEventAggregator, EventAggregator>(); // for event handling - singleton
+
+            // refelction to rope in view models on start up 
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // startup view model using caliburn and micro -  application will launch correct VM on startup
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        // override to use our DI container
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        // override to use our DI container
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        // override to use our DI container
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
