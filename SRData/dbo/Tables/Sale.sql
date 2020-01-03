9e8e94653147a6624fc0a112a1223931cffa4ba7
jamattard <jamattard@gmail.com>
2020-01-03 11:04:22 +0100

    Register DB Tables
    Basic initial tables

---
+++ SRData/dbo/Tables/Sale.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[CashierId] NVARCHAR(128) NOT NULL, 
+	[SaleDate] DATETIME2 NOT NULL,
+	[SubTotal] Money NOT NULL,
+	[Tax] Money NOT NULL,
+	[Total] Money NOT NULL,
+	[CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+
+)
